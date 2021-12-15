86ed2f9cc7a231991f0472ae6813907b5c44d719
Corentin LHERMITTE <corentin.l-hermitte@epitech.eu>
2021-12-15 20:14:17 +0100

    [add]

---
+++ pages/api/TopBar.js
@@ -0,0 +1,20 @@
+export default function TopBar() {
+    return (
+        <nav>
+        <ul>
+            <li>
+            <a href="default.asp">Home</a>
+            </li>
+            <li>
+            <a href="news.asp">News</a>
+            </li>
+            <li>
+            <a href="contact.asp">Contact</a>
+            </li>
+            <li>
+            <a href="about.asp">About</a>
+            </li>
+        </ul>
+        </nav>
+    );
+}
